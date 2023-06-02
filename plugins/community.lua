56031924936111b05df9c0d6da3c565d87ffd577
정형진 <88423849+KeMezz@users.noreply.github.com>
2023-06-02 17:00:21 +0900

    install packages

--- plugins/community.lua
+++ plugins/community.lua
@@ -6,4 +6,13 @@ return {
 
   -- { import = "astrocommunity.colorscheme.catppuccin" },
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
+  { import = "astrocommunity.motion.nvim-surround" },
+  { import = "astrocommunity.pack.typescript" },
+  { import = "astrocommunity.pack.tailwindcss" },
+  { import = "astrocommunity.pack.markdown" },
+  -- { import = "astrocommunity.colorscheme.tokyonight" },
+  -- { import = "astrocommunity.colorscheme.everforest" },
+  -- { import = "astrocommunity.colorscheme.nightfox" },
+  { import = "astrocommunity.colorscheme.catppuccin" },
+  { "catppuccin", opts = { flavour = "macchiato" } },
 }
